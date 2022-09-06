ae8b7c75bf35e4add6fcfc4959e756a2d9865340
TanTheta15 <100551214+TanTheta15@users.noreply.github.com>
2022-09-06 15:59:37 +0530

    Create Disjoint Set.cpp

---
+++ Disjoint Set.cpp
@@ -0,0 +1,24 @@
+const static int N = 1e5 + 10;
+int parent[N], rank[N];
+
+void make(int i)
+{
+	parent[i] = i;
+	rank[i] = 1;
+}
+
+int find(int i){
+	if(parent[i] == i) return i;
+	else parent[i] = find(parent[i]);
+}
+
+void Union(int a, int b){
+	a = find(a);
+	b = find(b);
+	if(a == b) return;
+
+	if(rank[b] > rank[a]) swap(a, b);
+	parent[b] = a;
+	rank[a] = rank[b] + 1;
+
+}
